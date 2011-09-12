0689bf71198291d50651528e73ed2399adc958f6
Jane <jday@engineyard.com>
2011-09-12 10:07:37 -0700

    Edited config.php via GitHub

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $config_site_name		= "Site Name";
 
-$db_hostname			= "localhost";
-$db_database			= "egypt";
-$db_username			= "root";
-$db_password			= "1234";
+$db_hostname			= "10.194.111.8";
+$db_database			= "db_214cbac6";
+$db_username			= "user_214cbac6";
+$db_password			= "-Xf.Y8JYF.At1T";
 $db_dbdriver			= "mysql";
